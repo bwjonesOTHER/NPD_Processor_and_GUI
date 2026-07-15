af08ffe1fd3faf77e75063a7544365a2f4f2b30d
tmoore05 <thomasjmoore870@gmail.com>
2026-07-15 16:07:10 -0600

    Fix missing OverTemp data by allowing search fallback to base dir if area folder is absent

--- backend/plot_generator.py
+++ backend/plot_generator.py
@@ -558,8 +558,8 @@ def generate_plots(params):
                     if os.path.isdir(os.path.join(base_d, d)) and pma_norm in d_norm:
                         return os.path.join(base_d, d)
                 # If a specific PMA Area was requested but we couldn't find its folder, 
-                # return a dummy path so we don't accidentally scan ALL areas in the root folder!
-                return os.path.join(base_d, "NON_EXISTENT_PMA_AREA_FALLBACK")
+                # just return the base directory (e.g. OverTemp might not have Area subfolders)
+                return base_d
             
             bench = get_subfolder(folderB, "bench")
             if bench:
