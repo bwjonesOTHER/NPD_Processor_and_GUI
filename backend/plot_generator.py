684204263774b4935e0b5f4805ce85e9cd5f5ad6
tmoore05 <thomasjmoore870@gmail.com>
2026-07-23 14:16:29 -0600

    Restore SpecA calibration for Test 3 NP

--- backend/plot_generator.py
+++ backend/plot_generator.py
@@ -97,10 +97,7 @@ def get_calibration_loss(filepath, cal_folder, test_type=1, plot_s12=False):
     ident_num = cap_match.group(1) if cap_match else None
 
     if is_npd:
-        if is_benchtop:
-            cal_types = ("Base", "Bulkhead")
-        else:
-            cal_types = ("Base", "Bulkhead", "SpecA")
+        cal_types = ("Base", "Bulkhead", "SpecA")
     else:
         # Only use Cap for Tile Benchtop (which has a Cap number). Arrays use Hat.
         if is_benchtop and ident_num is not None:
