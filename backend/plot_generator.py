915e7d4d342b02fd476bb88aed4602f6ddb916c4
tmoore05 <thomasjmoore870@gmail.com>
2026-07-23 13:33:02 -0600

    Disable SpecA for benchtop NP tests

--- backend/plot_generator.py
+++ backend/plot_generator.py
@@ -97,7 +97,10 @@ def get_calibration_loss(filepath, cal_folder):
     ident_num = cap_match.group(1) if cap_match else None
 
     if is_npd:
-        cal_types = ("Base", "Bulkhead", "SpecA")
+        if is_benchtop:
+            cal_types = ("Base", "Bulkhead")
+        else:
+            cal_types = ("Base", "Bulkhead", "SpecA")
     else:
         # Only use Cap for Tile Benchtop (which has a Cap number). Arrays use Hat.
         if is_benchtop and ident_num is not None:
